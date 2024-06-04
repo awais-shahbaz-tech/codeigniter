5442a764521c2a4056f2f45d220df3c305dcc77d
Awais Shahbaz <awaishahbaz@gmail.com>
2024-06-04 12:32:11 +0500

    change base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://outdoor-agnes-innovationtech-6e4d4308.koyeb.app';
+    public string $baseURL = 'https://outdoor-agnes-innovationtech-6e4d4308.koyeb.app/ci-practice';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
