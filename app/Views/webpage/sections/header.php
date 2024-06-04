21136b492d3748a484ea5a5a94e17b9528b87d83
Awais Shahbaz <awaishahbaz@gmail.com>
2024-06-04 12:53:09 +0500

    chnage url

--- app/Views/webpage/sections/header.php
+++ app/Views/webpage/sections/header.php
@@ -17,13 +17,13 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="<?php echo base_url();?>assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>public/assets/vendor/aos/aos.css" rel="stylesheet">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"></script>
    <!-- Include jQuery -->
